f435fb0ed91d5cdc4e9bc15a09402cbd14da82bf
JesperCheong <75248011+JesperCheong@users.noreply.github.com>
2024-04-16 11:42:10 +0000

    Added filter functionality

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ async function fetchTodo() {
     const data = await response.json();
     //organise data for ease of use later
     organizedData = data.map(datum => {
-      if (datum.completed === true) {
+      /* if (datum.completed === true) {
         datum.completed = "✔";
       } else if (datum.completed === false) {
         datum.completed = "";
-      }
+      } */
       return {
-        "id": datum.id,
-        "title": datum.title,
-        "userId": datum.userId,
-        "completed": datum.completed,
+        "id": String(datum.id),
+        "title": String(datum.title),
+        "userId": String(datum.userId),
+        "completed": String(datum.completed),
       };
     });
     todoList = organizedData;
@@ -22,8 +22,6 @@ async function fetchTodo() {
     alert(error);
   }
 }
-
-
 async function render() {
   await fetchTodo();
   renderUserFilter();
@@ -32,20 +30,46 @@ async function render() {
 }
 
 function renderList() {
+  //clear list
   const tableBody = document.getElementsByTagName("tbody");
-  todoList.forEach((todo) => {
+  while (tableBody[0].firstChild) {
+    tableBody[0].removeChild(tableBody[0].firstChild);
+  }
+  
+  // filter list
+  const filterStatusInput = statusFilter.value;
+  const filterUserInput = userFilter.value;
+
+  const filteredList = todoList.filter(todo => {
+    return (filterStatusInput === "All" || todo.completed === filterStatusInput) &&
+      (filterUserInput === "All" || todo.userId === filterUserInput);
+  })
+
+  // render filtered list
+  filteredList.forEach((todo) => {
     const row = document.createElement("tr");
+    // dislpay checkmark if todo is completed
     for (const item in todo) {
       const cell = document.createElement("td");
-      cell.textContent = todo[item];
+      if (item === "completed") {
+        if (todo[item] === "true") {
+          cell.textContent = "✔";
+        } else if (todo[item] === "false") {
+          cell.textContent = ""
+        }
+      } else {
+        cell.textContent = todo[item];
+      }
       row.appendChild(cell);
     }
     tableBody[0].appendChild(row);
   });
 }
 function renderUserFilter() {
+  // to eliminate duplicates & grab unique user id
   const uniqueUserIds = [...new Set(todoList.map(item => item.userId))];
-  const filterByUser = document.getElementById("filter-by-user");
+  // create options for filter
+  const filterByUser = document.getElementById("userFilter");
   uniqueUserIds.forEach((id) => {
     const option = document.createElement("option");
     option.value = id;
@@ -54,22 +78,27 @@ function renderUserFilter() {
   })
 }
 function renderStatusFilter() {
+  // to eliminate duplicates and grab unique status
   const uniqueStatus = [...new Set(todoList.map(item => item.completed))];
-  const filterByStatus = document.getElementById("filter-by-status");
+  // create options for filter
+  const filterByStatus = document.getElementById("statusFilter");
   uniqueStatus.forEach((status) => {
     const option = document.createElement("option");
-    if (status === "✔") {
-      option.value = "Completed";
-      option.textContent = "Completed";
-    } else if (status === "") {
-      option.value = "Incomplete";
-      option.textContent = "Incomplete";
+    if (status === "true") {
+      option.value = "true";
+      option.textContent = "Completed"; // for display
+    } else if (status === "false") {
+      option.value = "false";
+      option.textContent = "Incomplete"; /// for display
     }
-    
     filterByStatus.appendChild(option);
   })
 }
 
-
 let todoList;
-render();
+render(); // initial rendering
+
+const statusFilter = document.getElementById('statusFilter');
+const userFilter = document.getElementById('userFilter');
+statusFilter.addEventListener("change", renderList);
+userFilter.addEventListener("change", renderList);
